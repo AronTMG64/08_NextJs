3467d634c8b1454da93ef2f0f2c4a2f8cd4c60b6
AronTMG64 <a.grankulla@optistud.fi>
2023-05-11 13:19:09 +0300

    init

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import styles from './Container.module.css'
+import { ReactNode } from "react"
+
+export type ContainerProps = {
+  children?: ReactNode
+}
+
+export default function Container({children}: ContainerProps) {
+  return (
+    <div className={styles.container}>
+      {children}
+    </div>
+  )
+}
